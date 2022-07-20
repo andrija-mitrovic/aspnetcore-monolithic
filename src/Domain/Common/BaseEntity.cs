7b0fdea4fd8e665b1dfdebd43fb59217e900bb4e
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-20 08:55:42 +0200

    Add base entity

---
+++ src/Domain/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        private readonly List<BaseEvent> _domainEvents = new();
+    }
+}
