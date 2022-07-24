99d50f6bb551872c8179c4fb1805f942a67bdee0
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-24 08:30:23 +0200

    Add TodoItem created event

---
+++ src/Domain/Events/TodoItemCreatedEvent.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+using Domain.Entities;
+
+namespace Domain.Events
+{
+    public class TodoItemCreatedEvent : BaseEvent
+    {
+        public TodoItemCreatedEvent(TodoItem item)
+        {
+            Item = item;
+        }
+
+        public TodoItem Item { get; }
+    }
+}
