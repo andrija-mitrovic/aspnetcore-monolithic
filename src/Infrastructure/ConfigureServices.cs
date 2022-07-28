3929e6465cab0bce500f204583aa8ef7c6c16452
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-28 08:21:01 +0200

    Configure services in the infrastructure layer

---
+++ src/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,45 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Files;
+using Infrastructure.Identity;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Interceptors;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ConfigureServices
+    {
+        private const string CONNECTION_STRING_NAME = "DefaultConnection";
+
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString(CONNECTION_STRING_NAME),
+                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddDefaultIdentity<ApplicationUser>()
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer().AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+            services.AddScoped<ApplicationDbContextInitialiser>();
+
+            services.AddTransient<IDateTime, DateTimeService>();
+            services.AddTransient<IIdentityService, IdentityService>();
+            services.AddTransient<ICsvFileBuilder, CsvFileBuilder>();
+
+            services.AddAuthentication().AddIdentityServerJwt();
+
+            services.AddAuthorization(options => options.AddPolicy("CanPurge", policy => policy.RequireRole("Administrator")));
+
+            return services;
+        }
+    }
+}
