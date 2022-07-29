b053dd550e5da0f240d53b650e826a7266071b8d
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-29 08:33:33 +0200

    Add behaviours

---
+++ src/Application/Common/Behaviours/AuthorizationBehaviour.cs
@@ -0,0 +1,97 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Security;
+using MediatR;
+using System.Reflection;
+
+namespace Application.Common.Behaviours
+{
+    public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IIdentityService _identityService;
+
+        public AuthorizationBehaviour(
+            ICurrentUserService currentUserService, 
+            IIdentityService identityService)
+        {
+            _currentUserService = currentUserService;
+            _identityService = identityService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var authorizeAttributes = request.GetType().GetCustomAttributes<AuthorizeAttribute>();
+
+            if (authorizeAttributes.Any())
+            {
+                // Must be authenticated user
+                ThrowExceptionIfUserNotAuthenticated(_currentUserService.UserId);
+
+                // Role-based authorization
+                await HandleRoleAuthorization(authorizeAttributes, _currentUserService.UserId!);
+
+                // Policy-based authorization
+                await HandlePolicyAuthorization(authorizeAttributes, _currentUserService.UserId!);
+            }
+
+            // User is authorized / authorization not required
+            return await next();
+        }
+
+        private void ThrowExceptionIfUserNotAuthenticated(string? userId)
+        {
+            if (userId == null)
+            {
+                throw new UnauthorizedAccessException();
+            }
+        }
+
+        private async Task HandleRoleAuthorization(IEnumerable<AuthorizeAttribute> authorizeAttributes, string userId)
+        {
+            var authorizeAttributesWithRoles = authorizeAttributes.Where(a => !string.IsNullOrWhiteSpace(a.Roles));
+
+            if (authorizeAttributesWithRoles.Any())
+            {
+                var authorized = false;
+
+                foreach (var roles in authorizeAttributesWithRoles.Select(a => a.Roles.Split(',')))
+                {
+                    foreach (var role in roles)
+                    {
+                        var isInRole = await _identityService.IsInRoleAsync(userId, role.Trim());
+                        if (isInRole)
+                        {
+                            authorized = true;
+                            break;
+                        }
+                    }
+                }
+
+                // Must be a member of at least one role in roles
+                if (!authorized)
+                {
+                    throw new ForbiddenAccessException();
+                }
+            }
+        }
+
+        private async Task HandlePolicyAuthorization(IEnumerable<AuthorizeAttribute> authorizeAttributes, string userId)
+        {
+            var authorizeAttributesWithPolicies = authorizeAttributes.Where(a => !string.IsNullOrWhiteSpace(a.Policy));
+
+            if (authorizeAttributesWithPolicies.Any())
+            {
+                foreach (var policy in authorizeAttributesWithPolicies.Select(a => a.Policy))
+                {
+                    var authorized = await _identityService.AuthorizeAsync(userId, policy);
+
+                    if (!authorized)
+                    {
+                        throw new ForbiddenAccessException();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using Application.Common.Interfaces;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Common.Behaviours
+{
+    public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
+    {
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IIdentityService _identityService;
+        private readonly ILogger _logger;
+
+        public LoggingBehaviour(
+            ICurrentUserService currentUserService, 
+            IIdentityService identityService, 
+            ILogger logger)
+        {
+            _currentUserService = currentUserService;
+            _identityService = identityService;
+            _logger = logger;
+        }
+
+        public async Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            var userId = _currentUserService.UserId ?? string.Empty;
+            string userName = string.Empty;
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                userName = await _identityService.GetUserNameAsync(userId);
+            }
+
+            _logger.LogInformation("Request: {Name} {@UserId} {@UserName} {@Request}",
+                requestName, userId, userName, request);
+        }
+    }
+}
