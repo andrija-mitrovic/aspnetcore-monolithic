685514b1186b7e7f6ecd1791c5404daddffd53d9
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-30 08:36:51 +0200

    Add UpdateTodoItemDetail command

---
+++ src/Application/TodoItems/Commands/UpdateTodoItemDetail/UpdateTodoItemDetailCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.TodoItems.Commands.UpdateTodoItemDetail
+{
+    public class UpdateTodoItemDetailCommandHandler : IRequestHandler<UpdateTodoItemDetailCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateTodoItemDetailCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateTodoItemDetailCommand request, CancellationToken cancellationToken)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (todoItem == null)
+            {
+                throw new NotFoundException(nameof(TodoItem), request.Id);
+            }
+
+            todoItem.ListId = request.ListId;
+            todoItem.Priority = request.Priority;
+            todoItem.Note = request.Note;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
