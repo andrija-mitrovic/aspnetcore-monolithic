35077e7148eda815144495b91045889604b8e655
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-30 08:37:29 +0200

    Get TodoItems with pagination

---
+++ src/Application/TodoItems/Queries/GetTodoItemsWithPagination/GetTodoItemsWithPaginationQuery.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Models;
+using MediatR;
+
+namespace Application.TodoItems.Queries.GetTodoItemsWithPagination
+{
+    public class GetTodoItemsWithPaginationQuery : IRequest<PaginatedList<TodoItemBriefDto>>
+    {
+        public int ListId { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ src/Application/TodoItems/Queries/GetTodoItemsWithPagination/GetTodoItemsWithPaginationQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Mappings;
+using Application.Common.Models;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+
+namespace Application.TodoItems.Queries.GetTodoItemsWithPagination
+{
+    public class GetTodoItemsWithPaginationQueryHandler : IRequestHandler<GetTodoItemsWithPaginationQuery, PaginatedList<TodoItemBriefDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetTodoItemsWithPaginationQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<PaginatedList<TodoItemBriefDto>> Handle(GetTodoItemsWithPaginationQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.TodoItems.Where(x => x.ListId == request.ListId)
+                                           .OrderBy(x => x.Title)
+                                           .ProjectTo<TodoItemBriefDto>(_mapper.ConfigurationProvider)
+                                           .PaginatedListAsync(request.PageNumber, request.PageSize);
+        }
+    }
+}
