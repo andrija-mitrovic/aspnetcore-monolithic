d786837a87e662a513e46d8b654caa5b9ee5ac93
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-31 08:45:19 +0200

    Add export todos

---
+++ src/Application/TodoLists/Queries/ExportTodos/ExportTodosQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.TodoLists.Queries.ExportTodos
+{
+    public class ExportTodosQuery : IRequest<ExportTodosVm>
+    {
+        public int ListId { get; set; }
+    }
+}
