c79e97240068d92fb4d27a88436b863d81abaa00
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-31 08:44:23 +0200

    Add delete todolist

---
+++ src/Application/TodoLists/Commands/DeleteTodoList/DeleteTodoListCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Application.TodoLists.Commands.DeleteTodoList
+{
+    public record DeleteTodoListCommand(int Id) : IRequest;
+}
---
+++ src/Application/TodoLists/Commands/DeleteTodoList/DeleteTodoListCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.TodoLists.Commands.DeleteTodoList
+{
+    public class DeleteTodoListCommandHandler : IRequestHandler<DeleteTodoListCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteTodoListCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteTodoListCommand request, CancellationToken cancellationToken)
+        {
+            var todoList = await _context.TodoLists.Where(x => x.Id == request.Id).SingleOrDefaultAsync(cancellationToken);
+
+            if (todoList == null)
+            {
+                throw new NotFoundException(nameof(TodoList), request.Id);
+            }
+
+            _context.TodoLists.Remove(todoList);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
