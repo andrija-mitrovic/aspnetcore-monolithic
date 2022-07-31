80a18f50541428707d65d3716a905a44ee700382
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-31 08:44:07 +0200

    Add create todolist

---
+++ src/Application/TodoLists/Commands/CreateTodoList/CreateTodoListCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.TodoLists.Commands.CreateTodoList
+{
+    public class CreateTodoListCommand : IRequest<int>
+    {
+        public string? Title { get; set; }
+    }
+}
---
+++ src/Application/TodoLists/Commands/CreateTodoList/CreateTodoListCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.TodoLists.Commands.CreateTodoList
+{
+    public class CreateTodoListCommandHandler : IRequestHandler<CreateTodoListCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateTodoListCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateTodoListCommand request, CancellationToken cancellationToken)
+        {
+            var todoList = new TodoList();
+
+            todoList.Title = request.Title;
+
+            _context.TodoLists.Add(todoList);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return todoList.Id;
+        }
+    }
+}
