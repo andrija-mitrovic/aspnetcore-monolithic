a16a70c0e88736c0263a3ee637cd45213e80d687
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-07-31 08:44:37 +0200

    Add update todolist

---
+++ src/Application/TodoLists/Commands/UpdateTodoList/UpdateTodoListCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.TodoLists.Commands.UpdateTodoList
+{
+    public class UpdateTodoListCommandHandler : IRequestHandler<UpdateTodoListCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateTodoListCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateTodoListCommand request, CancellationToken cancellationToken)
+        {
+            var todoList = await _context.TodoLists.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (todoList == null)
+            {
+                throw new NotFoundException(nameof(TodoList), request.Id);
+            }
+
+            todoList.Title = request.Title;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
